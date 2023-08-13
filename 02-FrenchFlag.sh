a8cca8923384fdde79ae2905c74cf1f0787c8128
TuxStory <ajm.even@gmail.com>
2023-08-13 14:43:40 +0200

    02-FrenchFlag

---
+++ 02-FrenchFlag.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+##############################
+# Nom    : 02-FrenchFlag.sh #
+# Auteur : Antoine Even      #
+# Date   : 13/08/2023        #
+##############################
+
+############### Couleurs
+BLUE='\033[0;94m'
+WHITE='\033[0;97m'
+RED='\033[0;91m'
+
+############### Fonction
+function line() {
+  echo -e "${BLUE}"
+  printf "%0.s 0 1" {1..5}
+  echo -ne "${WHITE}"
+  printf "%0.s 0 1" {1..5}
+  echo -ne "${RED}"
+  printf "%0.s 0 1" {1..5}
+}
+
+############### Main
+for i in {1..20}; do line; done
+echo
